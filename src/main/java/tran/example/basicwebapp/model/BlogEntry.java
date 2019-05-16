1499a1726e1eeeebc4b40fab3a08024d87844382
jstran9 <wptran58@gmail.com>
2019-05-16 16:16:05 -0700

    creating a blogentry model object.

---
+++ src/main/java/tran/example/basicwebapp/model/BlogEntry.java
@@ -0,0 +1,13 @@
+package tran.example.basicwebapp.model;
+
+public class BlogEntry {
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
