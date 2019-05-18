8bb29516c1bd87b845511f50f3c52d5e89d9bef0
jstran9 <wptran58@gmail.com>
2019-05-17 23:14:04 -0700

    adding in annotations for persistence.

--- src/main/java/tran/example/basicwebapp/domain/Account.java
+++ src/main/java/tran/example/basicwebapp/domain/Account.java
@@ -1,7 +1,17 @@
 package tran.example.basicwebapp.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
     private String name;
     private String password;
 
