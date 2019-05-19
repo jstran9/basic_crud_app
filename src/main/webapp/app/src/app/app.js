3ceab83810436087a2b10df605d617a4b461cb4e
jstran9 <wptran58@gmail.com>
2019-05-19 00:18:50 -0700

    adding in the account module.

--- src/main/webapp/app/src/app/app.js
+++ src/main/webapp/app/src/app/app.js
@@ -3,6 +3,7 @@ angular.module( 'ngBoilerplate', [
   'templates-common',
   'ngBoilerplate.home',
   'ngBoilerplate.about',
+  'ngBoilerplate.account',
   'ui.router'
 ])
 
@@ -14,6 +15,11 @@ angular.module( 'ngBoilerplate', [
 })
 
 .controller( 'AppCtrl', function AppCtrl ( $scope, $location ) {
+  /*
+   * we set up an event handler below that changes the page title based on successful state change.
+   * the pageTitle is changed to the custom data specified in the state we're transitioning to.
+   * the state changes uses data binding.
+   */
   $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
     if ( angular.isDefined( toState.data.pageTitle ) ) {
       $scope.pageTitle = toState.data.pageTitle + ' | ngBoilerplate' ;
