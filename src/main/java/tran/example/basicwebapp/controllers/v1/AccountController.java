29fb4c9ea246e771868256b06de3876e4051e2d3
jstran9 <wptran58@gmail.com>
2019-05-21 17:11:06 -0700

    adding security annotations.

--- src/main/java/tran/example/basicwebapp/controllers/v1/AccountController.java
+++ src/main/java/tran/example/basicwebapp/controllers/v1/AccountController.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import tran.example.basicwebapp.api.v1.asm.AccountListResourceAsm;
@@ -18,6 +21,7 @@ import tran.example.basicwebapp.domain.Account;
 import tran.example.basicwebapp.domain.Blog;
 import tran.example.basicwebapp.exceptions.BadRequestException;
 import tran.example.basicwebapp.exceptions.ConflictException;
+import tran.example.basicwebapp.exceptions.ForbiddenException;
 import tran.example.basicwebapp.exceptions.NotFoundException;
 import tran.example.basicwebapp.services.AccountService;
 import tran.example.basicwebapp.services.exceptions.AccountDoesNotExistException;
@@ -42,6 +46,7 @@ public class AccountController {
     }
 
     @PostMapping
+    @PreAuthorize("permitAll")
     public ResponseEntity<AccountResource> createAccount(
             @RequestBody AccountResource sentAccount
     ) {
@@ -57,6 +62,7 @@ public class AccountController {
     }
 
     @GetMapping(value="/{accountId}")
+    @PreAuthorize("permitAll")
     public ResponseEntity<AccountResource> getAccount(
             @PathVariable Long accountId) {
         Account account = accountService.findAccount(accountId);
@@ -70,24 +76,38 @@ public class AccountController {
     }
 
     @PostMapping(value="/{accountId}/blogs")
+    @PreAuthorize("permitAll")
     public ResponseEntity<BlogResource> createBlog(
             @PathVariable Long accountId,
             @RequestBody BlogResource res)
     {
-        try {
-            Blog createdBlog = accountService.createBlog(accountId, res.toBlog());
-            BlogResource createdBlogRes = new BlogResourceAsm().toResource(createdBlog);
-            HttpHeaders headers = new HttpHeaders();
-            headers.setLocation(URI.create(createdBlogRes.getLink("self").getHref()));
-            return new ResponseEntity<>(createdBlogRes, headers, HttpStatus.CREATED);
-        } catch(AccountDoesNotExistException exception) {
-            throw new BadRequestException(exception);
-        } catch (BlogExistsException exception) {
-            throw new ConflictException(exception);
+        // we can also do the below using Annotations.
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if(principal instanceof UserDetails) {
+            UserDetails userDetails = (UserDetails) principal;
+            Account loggedIn = accountService.findByAccountName(userDetails.getUsername());
+            if(loggedIn.getId().equals(accountId)) {
+                try {
+                    Blog createdBlog = accountService.createBlog(accountId, res.toBlog());
+                    BlogResource createdBlogRes = new BlogResourceAsm().toResource(createdBlog);
+                    HttpHeaders headers = new HttpHeaders();
+                    headers.setLocation(URI.create(createdBlogRes.getLink("self").getHref()));
+                    return new ResponseEntity<>(createdBlogRes, headers, HttpStatus.CREATED);
+                } catch(AccountDoesNotExistException exception) {
+                    throw new BadRequestException(exception);
+                } catch (BlogExistsException exception) {
+                    throw new ConflictException(exception);
+                }
+            } else {
+                throw new ForbiddenException();
+            }
+        } else {
+            throw new ForbiddenException();
         }
     }
 
     @GetMapping
+    @PreAuthorize("permitAll")
     public ResponseEntity<AccountListResource> findAllAccounts(@RequestParam(value="name", required = false) String name) {
         AccountList list = null;
         if(name == null) {
@@ -105,6 +125,7 @@ public class AccountController {
     }
 
     @GetMapping(value="/{accountId}/blogs")
+    @PreAuthorize("permitAll")
     public ResponseEntity<BlogListResource> findAllBlogs(
             @PathVariable Long accountId) {
         try {
--- src/main/java/tran/example/basicwebapp/controllers/v1/BlogController.java
+++ src/main/java/tran/example/basicwebapp/controllers/v1/BlogController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import tran.example.basicwebapp.api.v1.asm.BlogEntryListResourceAsm;
@@ -35,6 +36,7 @@ public class BlogController {
     }
 
     @GetMapping
+    @PreAuthorize("permitAll")
     public ResponseEntity<BlogListResource> findAllBlogs()
     {
         BlogList blogList = blogService.findAllBlogs();
